8c2b0c47cb6a36e1dd685bb5ec2758ae68cb91a9
Chandrashekhar Garkar <cngarkar@gmail.com>
2018-12-23 14:30:55 -0600

    Lesson 5 - Create new activity

--- app/src/main/java/co/uttar/namaskarapplication/MainActivity.java
+++ app/src/main/java/co/uttar/namaskarapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package co.uttar.namaskarapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view)
+    {
+        Intent gotoHomeActivity = new Intent(this.getApplicationContext(), HomeActivity.class);
+        startActivity(gotoHomeActivity);
     }
 }
